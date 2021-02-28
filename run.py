c273d2ab17199b35493b029de6b24b55381e447d
Side-On <nathanhighams@gmail.com>
2021-02-28 16:08:17 +0000

    Deployment to heroku

--- run.py
+++ run.py
@@ -3,19 +3,24 @@ import json
 from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
-    
+
+
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route("/about")
 def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template('about.html', page_title="About", company=data)
+    return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -27,12 +32,14 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
+
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
-    return render_template('contact.html', page_title="Contact")
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
@@ -43,6 +50,4 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
-    )
-    
+        debug=True)
